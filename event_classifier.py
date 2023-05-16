fixed integer overflow

event_classifier.py
@@ -48,6 +48,7 @@ def predict_events(data, prev_sd, is_peak_found, window_size=10000, event_thresh
     while (lower_interval + 4000) < max_time:
         upper_interval = int(lower_interval + 10000)
         interval = data[lower_interval:upper_interval]
+        interval = np.array(interval, dtype=np.int64)
         print(lower_interval)
         if not is_event_change(interval, event_threshold):
             is_peak_found = False
@@ -64,13 +65,3 @@ def predict_events(data, prev_sd, is_peak_found, window_size=10000, event_thresh
         lower_interval = int(lower_interval + increment)
 
     return predicted_labels, prev_sd, is_peak_found # predicted_times
-
-sample_rate, wave_array = wavfile.read('BBB.wav')
-# predict_events(wave_array, 0, False)
-section = wave_array[47001:57001]
-import sys
-np.set_printoptions(threshold=sys.maxsize)
-print((statistics.mean(section**2))**(1/2))
-print(section**2)
-for i in range(1, len(section)):
-    print(type(section[i]))